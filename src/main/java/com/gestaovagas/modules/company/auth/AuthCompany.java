13d3e8ac696a3ca19c6e46ddc0859f2a404fa5d3
Victor <victor09ciechovicz@outlook.com>
2025-10-24 23:37:27 -0300

    refact code

--- src/main/java/com/gestaovagas/modules/company/auth/AuthCompany.java
+++ src/main/java/com/gestaovagas/modules/company/auth/AuthCompany.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,7 @@ public class AuthCompany {
     @Value("${sucury.token.secret}")
     private String secretKey;
 
-    public String execute(AuthCompanyDTO dto) throws AuthenticationException {
+    public String execute(AuthCompanyDTO dto) {
         var company = companyRepository.findByUsername(dto.getUsername()).orElseThrow(
                 () -> {
                     throw new UsernameNotFoundException("Username not found");
@@ -34,7 +35,7 @@ public class AuthCompany {
         var match = passwordEncoder.matches(dto.getPassword(), company.getPassword());
 
         if (!match) {
-            throw new AuthenticationException();
+            throw new BadCredentialsException("Senha inválida");
         }
 //se for igual gera um JWT token
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
--- src/main/java/com/gestaovagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/com/gestaovagas/modules/company/controllers/AuthCompanyController.java
@@ -4,13 +4,13 @@ package com.gestaovagas.modules.company.controllers;
 import com.gestaovagas.modules.company.auth.AuthCompany;
 import com.gestaovagas.modules.company.dto.AuthCompanyDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.naming.AuthenticationException;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,7 +21,13 @@ public class AuthCompanyController {
     private final AuthCompany authCompany;
 
     @PostMapping("/company")
-    public String create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
-        return authCompany.execute(authCompanyDTO);
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) {
+        try {
+            var result = authCompany.execute(authCompanyDTO);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+
     }
 }
