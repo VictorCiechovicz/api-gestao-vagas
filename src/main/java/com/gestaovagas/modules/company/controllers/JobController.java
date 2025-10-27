5e82a7ac28bae310aa360012f4edcc5f34d7d519
Victor <victor09ciechovicz@outlook.com>
2025-10-27 08:17:48 -0300

    feat:create filter jwt token

--- src/main/java/com/gestaovagas/modules/company/controllers/JobController.java
+++ src/main/java/com/gestaovagas/modules/company/controllers/JobController.java
@@ -3,6 +3,7 @@ package com.gestaovagas.modules.company.controllers;
 
 import com.gestaovagas.modules.company.entities.JobEntity;
 import com.gestaovagas.modules.company.services.JobService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -19,7 +22,9 @@ public class JobController {
     private final JobService jobService;
 
     @PostMapping("/create")
-    public ResponseEntity<Object> jobCompany(@Valid @RequestBody JobEntity jobEntity) {
+    public ResponseEntity<Object> jobCompany(@Valid @RequestBody JobEntity jobEntity, HttpServletRequest request) {
+        var companyId = request.getAttribute("company_id");
+        jobEntity.setCompanyId(UUID.fromString(companyId.toString()));
         return jobService.createJob(jobEntity);
 
     }
--- src/main/java/com/gestaovagas/modules/company/repository/JobRepository.java
+++ src/main/java/com/gestaovagas/modules/company/repository/JobRepository.java
@@ -3,7 +3,6 @@ package com.gestaovagas.modules.company.repository;
 import com.gestaovagas.modules.company.entities.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
